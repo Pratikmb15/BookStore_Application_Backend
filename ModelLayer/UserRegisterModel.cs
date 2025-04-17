24fd76703e1ae27370c75a06b9f11cbf10ef6b6d
pratikbagul15 <pratikbagul1508@gmail.com>
2025-04-17 10:51:16 +0530

    first commit

---
+++ ModelLayer/UserRegisterModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    class UserRegisterModel
+    {
+        public string fullName { get; set; }
+        [Required, EmailAddress]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required, Phone]
+        public string mobileNum { get; set; }
+        [Required]
+        public string? role { get; set; } = "User";
+    }
+}
